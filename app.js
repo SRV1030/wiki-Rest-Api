0d3ed5181b1d07e0ad2a5a00ff74789b7a645fc6
SRV1030 <65103790+SRV1030@users.noreply.github.com>
2021-05-10 14:37:02 +0545

    changes

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const express= require("express")
-const bodyParser= require("body-parser");
-const ejs= require("ejs");
-const mongoose= require("mongoose");
-const app=express();
+const express = require("express")
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const app = express();
 
 const port = process.env.PORT || 3000;
 
@@ -12,33 +12,87 @@ app.use('/static', express.static('static'));
 
 app.set('view engine', 'ejs');
 
-mongoose.connect("mongodb://localhost:27017/wikiDB",{useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParser: true });
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`localhost:${port}`);
 })
 
-const articleSchema= new mongoose.Schema({
-    title:String,
-    content:String
+const articleSchema = new mongoose.Schema({
+    title: String,
+    content: String
 });
 
-const Article= mongoose.model('Article',articleSchema);
+const Article = mongoose.model('Article', articleSchema);
 
-app.get("/articles",(req,res)=>{
-    Article.find(function(err,foundArticles){
-        if(!err)res.send(foundArticles);
-        else res.send(err);
+/////////////////Requests for all the articles///////////////////////////
+app.route("/articles")
+    .get((req, res) => {
+        Article.find(function (err, foundArticles) {
+            if (!err) res.send(foundArticles);
+            else res.send(err);
+        })
     })
-})
 
-app.post("/articles",(req,res)=>{
-    const newArticle= new Article({
-        title:req.body.title,
-        content:req.body.content
+    .post((req, res) => {
+        const newArticle = new Article({
+            title: req.body.title,
+            content: req.body.content
+        });
+        newArticle.save((err) => {
+            if (!err) res.send("successfully added new article");
+            else res.send(err);
+        });
+    })
+
+    .delete((req, res) => {
+        Article.deleteMany(function (err) {
+            if (!err) res.send("successfully deleted all articles");
+            else res.send(err);
+        });
     });
-    newArticle.save((err)=>{
-        if(!err)res.send("success");
-        else res.send(err);
+
+/////////////////Requests for specific articlse///////////////////////////
+
+app.route("/articles/:articleTitle")
+    .get((req, res) => {
+        Article.findOne({ title: req.params.articleTitle },
+            function (err, foundArticle) {
+                if (!err) {
+                    if (foundArticle) res.send(foundArticle);
+                    else res.send("Not found");
+                }
+                else res.send(err);
+            });
+    })
+    .put((req, res) => {
+        Article.update(
+            { title: req.params.articleTitle },
+            {
+                title: req.body.title,
+                content: req.body.content
+            },
+            { overwrite: true },
+            function (err) {
+                if (!err) res.send("Successfully updated");
+                else res.send(err);
+            }
+        )
+    })
+    .patch((req, res) => {
+        Article.update(
+            { title: req.params.articleTitle },
+            { $set: req.body },
+            function (err) {
+                if (!err) res.send("Successfully updated");
+                else res.send(err);
+            }
+        );
+    })
+    .delete((req, res) => {
+        Article.deleteOne({ title: req.params.articleTitle },
+            function (err) {
+                if (!err) res.send("successfully deleted article");
+                else res.send(err);
+            });
     });
-})
